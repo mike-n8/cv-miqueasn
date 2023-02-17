79bb82cd2e9b193bf98d5d7b1631f86c33c2547d
mike-n8 <miqueas_mirt@hotmail.com>
2023-02-17 19:43:43 -0300

    Se agrega el Proyecto

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+
+let btnDes0 = document.querySelector("#breve-des-0");
+let btnDes1 = document.querySelector(".breve-des-1");
+let btnDes2 = document.querySelector(".breve-des-2");
+let btnDes3 = document.querySelector(".breve-des-3");
+let btnDes4 = document.querySelector(".breve-des-4");
+
+function verParrafo() {
+    if (btnDes0.style.display == "none") {
+        btnDes0.style.display="block";
+    } else {
+        btnDes0.style.display="none";
+    }
+}
+
+function verParrafo1() {
+    if (btnDes1.style.display == "none") {
+        btnDes1.style.display="block";
+    } else {
+        btnDes1.style.display="none";
+    }
+}
+
+function verParrafo2() {
+    if (btnDes2.style.display == "none") {
+        btnDes2.style.display="block";
+    } else {
+        btnDes2.style.display="none";
+    }
+}
+
+function verParrafo3() {
+    if (btnDes3.style.display == "none") {
+        btnDes3.style.display="block";
+    } else {
+        btnDes3.style.display="none";
+    }
+}
+
+function verParrafo4() {
+    if (btnDes4.style.display == "none") {
+        btnDes4.style.display="block";
+    } else {
+        btnDes4.style.display="none";
+    }
+}
